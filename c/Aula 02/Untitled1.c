0bda67032ffc9fbe011ca869373cad8b0edd1aac
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-08-19 19:27:36 -0300

    a

---
+++ c/Aula 02/Untitled1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World\n");
+
+  // Toda função em C precisa de um retorno
+  // e main é uma função
+  return 0;
+
+  // return 0 significa que a função executou com
+  // êxito.
+}
