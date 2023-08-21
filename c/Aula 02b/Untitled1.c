a5ced27a586b63569ebabbf64c5dcc30157219fc
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-08-20 23:53:53 -0300

    a

--- c/Aula 02b/Untitled1.c
+++ c/Aula 02b/Untitled1.c
@@ -1,9 +1,31 @@
-#include<Stdio.h>
+#include<stdio.h>
 
 
 void main()
 {
-    printf("Hello World!");
+
+    // Saída Padrão
+    printf("Hello World \n");
+
+    // Se eu quiser mostrar valores de operações
+    // %d para valores inteiros
+    // printf("%d", valor);
+
+    // é combinado em ordem.
+    printf("%d, com %d, resulta em %d", 5, 3, 5+3);
+
+    // Para valores reais, usamos %f
+
+    // Para limitar as casas decimais, usamos .valor
+    // entre % e f (%.2f) - limita a duas casas decimais
+    printf("%f, com %f, resulta em %.2f", 5, 3, 5+3);
+
+
+    /*
+    A ordem de operação é PEMDAS
+    ou da esquerda para a direita
+    */
+
 
     return 0;
 }
