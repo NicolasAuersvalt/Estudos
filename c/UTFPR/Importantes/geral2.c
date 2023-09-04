257e8d80be2e959b4a5e2b8e64755b996b61045d
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-09-03 23:56:20 -0300

    Add files via upload

---
+++ c/UTFPR/Importantes/geral2.c
@@ -0,0 +1,59 @@
+/*
+=====/=====/=====/=====/=====/=====/=====/=====/=====/=====
+
+1-2° Aula
+
+%d - Decimal
+%f - Float
+%c - char
+
+Basta que um valor esteja em float para ter precisão (5/2.0 ou 5.0/2)
+
+A alocação de %d é diferente de %f
+%d != %f (5 != 5.0)
+
+scanf(, &) // & é o endereço
+
+
+Se colocar "%d oi", ele vai esperar digitar um inteiro + "oi"
+
+Se colocar "%d, %d", ele vai esperar ler a vírgula
+
+Se colocar "%d ", ele vai esperar ler um espaço
+
+Deve-se atribuir um valor à variável antes de usá-la
+O destino fica sempre à esquerda, e o valor a armazenar fica sempre à direita!
+int = 1;
+
+
+\n - Nova linha
+\t - Tabulação horizontal
+\a - Sinal de alerta sonoro
+\\ - Barra invertida
+
+
+Se os dois argumentos forem inteiros, acontece a divisão
+inteira.
+
+Se ao menos um dos dois argumentos for um valor real,
+acontece a divisão de ponto flutuante. (10/2.0) = (5.000000)
+
+3° Aula
+
+  Os operadores relacionais retornam 0 para falso e 1 para verdadeiro
+
+  == igual
+  != diferente
+  < menor
+  <= menor ou igual
+  > maior
+  >= maior ou igual
+
+  || - ou
+  && - e
+  ! - NOT
+
+=====/=====/=====/=====/=====/=====/=====/=====/=====/=====
+
+
+  */
