0915d52616ffa3e899a2ebde2d27346014c5b381
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-09-03 23:24:11 -0300

    Add files via upload

---
+++ c/aulas/Importantes/Geral2.c
@@ -0,0 +1,27 @@
+/*
+=====/=====/=====/=====/=====/=====/=====/=====/=====/=====
+
+1° Aula
+
+%d - Decimal
+%f - Float
+%c - char
+
+Basta que um valor esteja em float para ter precisão (5/2.0 ou 5.0/2)
+
+A alocação de %d é diferente de %f
+%d != %f (5 != 5.0)
+
+scanf(, &) // & é o endereço
+
+
+Se colocar "%d oi", ele vai esperar digitar um inteiro + "oi" 
+
+Se colocar "%d, %d", ele vai esperar ler a vírgula
+
+Se colocar "%d ", ele vai esperar ler um espaço
+
+=====/=====/=====/=====/=====/=====/=====/=====/=====/=====
+  
+  
+  */
