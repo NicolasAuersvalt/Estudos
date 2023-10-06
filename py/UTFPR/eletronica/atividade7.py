7c74af964975dd253ff3319d9390147f00decc3e
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-10-06 17:30:28 -0300

    Update atividade7.py

--- py/UTFPR/eletronica/atividade7.py
+++ py/UTFPR/eletronica/atividade7.py
@@ -1,34 +1,35 @@
 # Testar em: https://www.programiz.com/python-programming/online-compiler/
+print("Atenção nas unidades!\n")
 
-print("Digite a capacitância: \n")
+print("Digite a capacitância (F): \n")
 c = float(input())
 
-print("Digite 1° resitência: \n")
+print("Digite 1° resitência (Ω): \n")
 ra = float(input())
 
-print("Digite 2° resistência: \n")
+print("Digite 2° resistência (Ω): \n")
 rb = float(input())
 
 
-th = ((0.6931) * (ra+rb) * c) # Correto
+th = float(((0.6931) * (ra+rb) * c)) # TESTADO
 
-tl = ((0.6931)  * rb * c) #  Correto
+tl = float(((0.6931)  * rb * c)) #  TESTADO
 
-t = th+tl #  Correto
+t = float(th+tl) #  TESTADO
 
-f = (1/t) #  Correto
+f = float(1/t) #  TESTADO
 
-d = (th/t) #  Correto
+d = float(th/t) #  Correto
 
-print("O valor do TH é de: {:.3f}s\n".format(th))
+print("O valor do TH é de aproximadamente: {:.3f}s\n".format(th))
 
-print("O valor de TL é: {:.3f}s\n".format(tl))
+print("O valor de TL é de aproximadamente: {:.3f}s\n".format(tl))
 
-print("O valor da f é: {:.3f}s\n".format(f))
+print("O valor da f é de aproximadamente: {:.3f}Hz\n".format(f))
 
-print("O valor de T é: {:.3f}v\n".format(t))
+print("O valor de T é de aproximadamente: {:.3f}v\n".format(t))
 
-print("O valor de D é: {:.3f}% \n".format(d))
+print("O valor de D é de aproximadamente: {:.3f}% \n".format(d))
 
 print("Digite BÃO para sair: ")
 a = input()
